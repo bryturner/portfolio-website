9ea1854262a9b41854f30e10d96c8fbb7ff47923
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-08-24 11:53:29 -0600

    reformat landing

--- scripts/script.js
+++ scripts/script.js
@@ -1,55 +1,37 @@
-'use strict';
+// 'use strict';
 
-//  Toggle light/dark page mode
-const toggleModeChk = document.getElementById('toggle-mode-checkbox');
+// // Sticky navigation
+// window.onscroll = () => {
+//   shrinkHeader();
+// };
 
-const toggleModeIcon = () => {
-  const modeIcon = document.getElementById('mode-icon');
+// const shrinkHeader = () => {
+//   const header = document.querySelector('.header__container');
 
-  if (toggleModeChk.checked) {
-    modeIcon.src = 'images/mode-icon-black.svg';
-  } else {
-    modeIcon.src = 'images/mode-icon-white.svg';
-  }
-};
+//   document.body.scrollTop > 0 || document.documentElement.scrollTop > 0
+//     ? header.classList.add('header-shrink')
+//     : header.classList.remove('header-shrink');
+// };
 
-toggleModeChk.addEventListener('change', () => {
-  document.body.classList.toggle('dark-theme');
-  toggleModeIcon();
-});
+// // Scroll to page section
+// const navLinks = document.querySelectorAll('.link-scroll');
 
-// Sticky navigation
-window.onscroll = () => {
-  shrinkHeader();
-};
+// navLinks.forEach(link => {
+//   link.addEventListener('click', e => {
+//     e.preventDefault();
+//     const href = link.getAttribute('href');
 
-const shrinkHeader = () => {
-  const header = document.querySelector('.header__container');
+//     // To top
+//     if (href === '#')
+//       window.scrollTo({
+//         top: 0,
+//         behavior: 'smooth',
+//       });
 
-  document.body.scrollTop > 0 || document.documentElement.scrollTop > 0
-    ? header.classList.add('header-shrink')
-    : header.classList.remove('header-shrink');
-};
-
-// Scroll to page section
-const navLinks = document.querySelectorAll('.link-scroll');
-
-navLinks.forEach(link => {
-  link.addEventListener('click', e => {
-    e.preventDefault();
-    const href = link.getAttribute('href');
-
-    // To top
-    if (href === '#')
-      window.scrollTo({
-        top: 0,
-        behavior: 'smooth',
-      });
-
-    // To other sections
-    if (href !== '#' && href.startsWith('#')) {
-      const sectionStart = document.querySelector(href);
-      sectionStart.scrollIntoView({ behavior: 'smooth' });
-    }
-  });
-});
+//     // To other sections
+//     if (href !== '#' && href.startsWith('#')) {
+//       const sectionStart = document.querySelector(href);
+//       sectionStart.scrollIntoView({ behavior: 'smooth' });
+//     }
+//   });
+// });
